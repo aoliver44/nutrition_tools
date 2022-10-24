75a7cc4e8454a93179c7a2dcf74a5dd1c087605a
aoliver44 <andrew.scott.oliver@gmail.com>
2022-10-24 10:38:05 -0700

    Added a cool tsne plot

--- prepare_foodtree_for_hfe.R
+++ prepare_foodtree_for_hfe.R
@@ -1,8 +1,8 @@
 ## prepare food tree file
 
-food_tree <- read_delim(file = "/home/data/synthetic_test_data/fl100_otu_abundance.txt", delim = "\t")
+food_tree_raw <- read_delim(file = "/home/data/food_tree_data/fl100_otu_abundance.txt", delim = "\t")
 
-food_tree <- food_tree %>% rename(., "clade_name" = "taxonomy") %>% select(., -`#FoodCode`) %>% relocate(., clade_name)
+food_tree <- food_tree_raw %>% rename(., "clade_name" = "taxonomy") %>% select(., -`#FoodCode`) %>% relocate(., clade_name)
 food_tree$clade_name <- gsub(pattern = ";", replacement = "\\|", x = food_tree$clade_name)
 
 food_tree <- food_tree %>% 
@@ -26,16 +26,92 @@ food_tree <- food_tree %>%
 
 ## identify the taxa group using L1-6
 food_tree$kingdom <- paste0("L1_", food_tree$kingdom)
-food_tree$phylum <- paste0("L2_", food_tree$phylum)
-food_tree$class <- paste0("L3_", food_tree$class)
-food_tree$order <- paste0("L4_", food_tree$order)
-food_tree$family <- paste0("L5_", food_tree$family)
-food_tree$genus <- paste0("L6_", food_tree$genus)
-
-## paste it all together
-food_tree$taxonomy <- paste0(food_tree$kingdom, "|", food_tree$phylum, "|", food_tree$class, "|", food_tree$order, "|", food_tree$family, "|", food_tree$genus)
+food_tree$phylum <- paste0(food_tree$kingdom, "|L2_", food_tree$phylum)
+food_tree$class <- paste0(food_tree$phylum, "|L3_", food_tree$class)
+food_tree$order <- paste0(food_tree$class, "|L4_", food_tree$order)
+food_tree$family <- paste0(food_tree$order, "|L5_", food_tree$family)
+food_tree$genus <- paste0(food_tree$family, "|L6_", food_tree$genus)
 
 
-food_tree <- food_tree %>% dplyr::select(., 7:last_col()) %>% relocate(., taxonomy) %>% rename(., "clade_name" = "taxonomy")
+food_tree_1 <- food_tree %>% group_by(., kingdom) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "kingdom")
+food_tree_2 <- food_tree %>% group_by(., phylum) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "phylum")
+food_tree_3 <- food_tree %>% group_by(., class) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "class")
+food_tree_4 <- food_tree %>% group_by(., order) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "order")
+food_tree_5 <- food_tree %>% group_by(., family) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "family")
+food_tree_6 <- food_tree %>% group_by(., genus) %>% dplyr::summarise(., across(where(is.numeric), ~ sum(.))) %>% dplyr::rename(., "clade_name" = "genus")
+
+food_tree <- rbind(food_tree_1, food_tree_2, food_tree_3, food_tree_4, food_tree_5, food_tree_6)
 
 write_delim(x = food_tree, file = "/home/data/synthetic_test_data/foodtree_for_hfe.txt", delim = "\t")
+
+
+## Nutrient tree ====================
+
+food_codes <- read_delim(file = "/home/data/food_tree_data/fl100_newick_taxonomy_nowater.txt", delim = "\t")
+food_tree <- read_delim(file = "/home/data/food_tree_data/fl100_otu_abundance.txt", delim = "\t")
+fndds <- readxl::read_xlsx(path = "/home/data/food_tree_data/2015-2016 FNDDS At A Glance - FNDDS Nutrient Values.xlsx", skip = 1)
+
+## merge food otu (original) and food codes
+food_tree_code <- merge(food_codes, food_tree, by.x = "Main.food.description", by.y = "#FoodCode")
+food_tree_code <- food_tree_code %>% dplyr::select(., FoodID, taxonomy.y) %>% rename(., "taxonomy" = "taxonomy.y")
+food_tree_code <- food_tree_code %>% separate(., col = "taxonomy", into = c("L1", "L2", "L3", "L4", "L5", "L6"), extra = "merge", sep = ";")
+
+## merge with nutritional data
+food_nutrition <- merge(food_tree_code, fndds, by.x = "FoodID", by.y = "Food code")
+food_nutrition <- food_nutrition %>% dplyr::select(., 1, 11:last_col())
+food_nutrition$FoodID <- paste0("fc_", food_nutrition$FoodID)
+
+food_nutrition <- food_nutrition %>% tibble::column_to_rownames(., "FoodID")
+
+## scale data
+food_nutrition_scaled <- scale(food_nutrition)
+
+## vis of MDS of the data
+set.seed(seed = 999)
+beta.mds <- vegan::metaMDS(food_nutrition_scaled, distance = 'euclidean')
+vegan::stressplot(beta.mds)
+
+sites <- as.data.frame(scores(beta.mds, display = "sites"))
+food_tree_code_data <- food_tree_code
+food_tree_code_data$FoodID <- paste0("fc_", food_tree_code_data$FoodID)
+nmds.sites <- merge(sites, food_tree_code_data, by.x = "row.names", by.y = "FoodID")
+
+ggplot() + 
+  geom_point(data = nmds.sites, aes(NMDS1, NMDS2, color = L1), size = 3, alpha = 0.8) + 
+  theme_bw() + theme(panel.border = element_rect(colour = "black", size = 1.5), 
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank()) + scale_color_d3()
+
+beta.mds$stress
+
+## tsne ======================
+
+library("Rtsne")
+tSNE_fit <- food_nutrition_scaled %>%
+  Rtsne(check_duplicates = FALSE, exaggeration_factor = 12)
+
+tSNE_df <- tSNE_fit$Y %>% 
+  as.data.frame() %>%
+  rename(tSNE1="V1",
+         tSNE2="V2") %>%
+  mutate(ID=row_number())
+
+food_tree_code_data <- food_tree_code_data[food_tree_code_data$FoodID %in% rownames(food_nutrition_scaled), ]
+food_tree_code_data <- food_tree_code_data[order(food_tree_code_data$FoodID, rownames(food_nutrition_scaled)), ]
+food_tree_code_data <- food_tree_code_data %>% mutate(ID=row_number())
+
+tSNE_df <- tSNE_df %>%
+  inner_join(food_tree_code_data, by="ID")
+
+tSNE_df %>%
+  dplyr::filter(., L1 == "L1_Sugars_Sweets_and_Beverages") %>%
+  ggplot(aes(x = tSNE1, 
+             y = tSNE2,
+             color = L3))+
+  geom_point()+
+  theme_bw() + 
+  #theme(legend.position="none") + 
+  scale_color_d3()
+
+food_tree_code_data %>% group_by(., L1) %>% tally
+
