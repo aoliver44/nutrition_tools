93c7e64098134a877c4676868fe0315dc8bbed64
aoliver44 <andrew.scott.oliver@gmail.com>
2023-01-09 14:20:38 -0800

    Make dietML modular, to handle more models

--- dietML.R
+++ dietML.R
@@ -26,8 +26,9 @@ Options:
     --label=<label> name of column that you are prediction [default: label]
     --cor_level level to group features together [default: 0.80]
     --train_split what percentage of samples should be used in training [default: 0.70]
-    --type are you trying to do classification (discrete levels of label) or regression (continous) [default: classification]
-    --seed random seed for reproducible results [default: 42]
+    --model what model would you like run (options: rf,lasso,ridge,enet) [defualt: rf]
+    --type for models that do both regression and classification [default: classification]
+    --seed set random seed [default: 42]
     --tune_length number of hyperparameter combinations to sample [default: 30]
     --ncores number of processesing cores for parallel computing [default: 2]
     
@@ -50,20 +51,21 @@ library(mikropml, quietly = T, verbose = F, warn.conflicts = F)
 ## helper functions ============================================================
 ## Negate function ("not in"):
 `%!in%` <- Negate(`%in%`)
-
+models <- c("rf", "lasso", "ridge", "enet")
 ## suppress warnings
 options(warn=-1)
 
-# opt <- data.frame(cor_level=numeric(),
-#                   label=character(),
-#                   train_split=numeric(),
-#                   seed=numeric(),
-#                   type=character(),
-#                   ncores=numeric(),
-#                   tune_length=numeric(),
-#                   input=character(),
-#                   outdir=character())
-# opt <- opt %>% tibble::add_row(cor_level = 0.80, train_split= 0.7, seed= 42, ncores = 4, tune_length = 100, label = c("feature_of_interest"), type= c("regression"), input = c("/home/output_old/butyrate_metaphlan4.txt"), outdir="/home/simulated_output/ml_results/")
+opt <- data.frame(cor_level=numeric(),
+                  label=character(),
+                  train_split=numeric(),
+                  seed=numeric(),
+                  model=character(),
+                  type=character(),
+                  ncores=numeric(),
+                  tune_length=numeric(),
+                  input=character(),
+                  outdir=character())
+opt <- opt %>% tibble::add_row(cor_level = 0.80, train_split= 0.7, model = "enet", seed= 42, ncores = 4, tune_length = 200, label = c("species"), type= c("classification"), input = c("/home/simulated_iris/iris.csv"), outdir="/home/simulated_iris/ml_results/")
 
 
 ## check for inputs ============================================================
@@ -145,129 +147,25 @@ test_data$id_tmp <- NULL
 
 set.seed(opt$seed)
 
-## Determine if your data is highly correlated =================================
+## run chosen model ============================================================
 
-## This is necessary because if, during preprocessing, you lose a lot of samples
-## this can cause weird behavior in hyperparameter searches. We are gonna
-## hedge agaist this.
-
-## co-correlate features at specified threshold
-training_cor <- mikropml:::group_correlated_features(train_data, 
-                                                 corr_thresh = as.numeric(opt$cor_level), group_neg_corr = T)
-
-## make dataframe of what is correlated at specified threshold.
-training_cor <- as.data.frame(training_cor) %>% 
-  tidyr::separate(., col = training_cor, into = c("keep", "co_correlated"), sep = "\\|", extra = "merge")
-
-## if we think a lot of samples (greater than 10% of all samples) will get 
-## dropped due to pre-processing steps, especially correlation, then we 
-## constrict the search space so that ranger doesnt error out
-
-## if you are dropping a lot due to correlation
-if (length(na.omit(training_cor$co_correlated)) > (NROW(training_cor) * 0.1)) {
-  if (opt$type == "classification") {
-    tuneGrid <-  expand.grid(.mtry = 2:((round((NROW(training_cor) * 0.9), digits = 0)) - (length(na.omit(training_cor$co_correlated)))),
-                             .splitrule = c("gini", "extratrees"),
-                             .min.node.size = 2:(min(20,(round((NROW(training_cor) * 0.9), digits = 0)) - (length(na.omit(training_cor$co_correlated))))))
-    
-    tuneGrid <- sample_n(tuneGrid, pmin(as.numeric(opt$tune_length), NROW(tuneGrid)))
-  } else {
-    
-    tuneGrid <-  expand.grid(.mtry = 2:((round((NROW(training_cor) * 0.9), digits = 0)) - (length(na.omit(training_cor$co_correlated)))),
-                             .splitrule = c("variance", "extratrees", "maxstat"),
-                             .min.node.size = 2:(min(20,(round((NROW(training_cor) * 0.9), digits = 0)) - (length(na.omit(training_cor$co_correlated))))))
-    
-    tuneGrid <- sample_n(tuneGrid, pmin(as.numeric(opt$tune_length), NROW(tuneGrid)))
-    
-  }
-  
-  ## else you are not dropping a lot due to correlation
-} else {
-  if (opt$type == "classification") {
-  tuneGrid <-  expand.grid(.mtry = 2:(round((NROW(training_cor) * 0.9), digits = 0)),
-                           .splitrule = c("gini", "extratrees"),
-                           .min.node.size = 2:(min(20,round((NROW(training_cor) * 0.9), digits = 0))))
-  
-  tuneGrid <- sample_n(tuneGrid, pmin(as.numeric(opt$tune_length), NROW(tuneGrid)))
-  } else {
-    
-    tuneGrid <-  expand.grid(.mtry = 2:(round((NROW(training_cor) * 0.9), digits = 0)),
-                             .splitrule = c("variance", "extratrees", "maxstat"),
-                             .min.node.size = 2:(min(20,round((NROW(training_cor) * 0.9), digits = 0))))
-    
-    tuneGrid <- sample_n(tuneGrid, pmin(as.numeric(opt$tune_length), NROW(tuneGrid)))
-    
-  }
+## check if user input model
+if (opt$model %!in% models) {
+  cat("\n#########################\n")
+  cat("ERROR: model not found", "\n")
+  cat("Please choose one of the following models for --model ", "\n")
+  print(as.data.frame(models))
+  cat("#########################\n\n")
 }
 
+## random forest
+if (opt$model %in% c("ranger", "rf", "randomforest")) {
+  source("/home/scripts/models/dietML_ranger.R")
+}
 
-## CV on training data =========================================================
-
-cat("\n#########################\n")
-cat("Note: Beginning ML (", opt$type, ") ...", "\n")
-cat("Preprocessesing includes near-zero variance filter and correlation threshold at ", opt$cor_level, "pearson.", "\n")
-cat("#########################\n\n")
-
-## GRID SEARCH (of randomly defined grid)
-
-fit_control <- caret::trainControl(method = "repeatedcv",
-                                   number = 10,
-                                   repeats = 3,
-                                   preProcOptions = list(cutoff = as.numeric(opt$cor_level)),
-                                   classProbs = TRUE,
-                                   search = "grid",
-                                   savePredictions = T,
-                                   allowParallel = TRUE,
-                                   verboseIter = TRUE)
-
-
-## make parallel jobs for model training
-cl <- parallel::makePSOCKcluster(as.numeric(opt$ncores))
-doParallel::registerDoParallel(cl)
-
-if (opt$type == "classification") {
-  
-  ## build models
-  training_fit <- caret::train(x = train_data, y = as.factor(train_label$label), 
-                               preProcess = c("nzv","corr"),
-                               method = "ranger", 
-                               trControl = fit_control, 
-                               tuneGrid = tuneGrid,
-                               importance = "permutation"
-  )
-  ## stop parallel jobs
-  parallel::stopCluster(cl)
-  
-  print(caret::confusionMatrix(data = predict(training_fit, test_data), reference = as.factor(test_label$label)))
-  
-  if (length(levels(as.factor(train_label$label))) == 2) {
-    ## run MLeval
-    pdf(file = paste0(opt$outdir, "roc_auc_curve.pdf"), width=5, height=5)
-    res <- MLeval::evalm(training_fit, plots = "r")
-    suppressMessages(dev.off())
-    
-  }
-} else if (opt$type == "regression") {
-  
-  ## build models
-  training_fit <- caret::train(x = train_data, y = as.numeric(train_label$label), 
-                               preProcess = c("nzv","corr"),
-                               method = "ranger", 
-                               trControl = fit_control, 
-                               tuneGrid = tuneGrid,
-                               importance = "permutation"
-  )
-  ## stop parallel jobs
-  parallel::stopCluster(cl)
-  
-  ## For regression:
-  hfe_pred <- predict(training_fit, test_data)
-  caret::postResample(pred = hfe_pred, obs = test_label$label)
-  
-} else {
-  ## error out
-  stop("--type <classification/regression> not found.")
-  
+## lasso/ridge/elastic net models
+if (opt$model %in% c("lasso", "ridge", "enet", "elasticnet")) {
+  source("/home/scripts/models/dietML_glmnet.R")
 }
 
 ## make training fit plots
