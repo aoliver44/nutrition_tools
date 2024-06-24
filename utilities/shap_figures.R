0d9643ac44256e0681ae7ae7f021abf6e6c3154a
aoliver44 <andrew.scott.oliver@gmail.com>
2024-06-24 10:49:45 -0700

    Massively remove redundant code
    use to calculate shap twice for full, train, test...just to make sure the figure could be generated both ways. Thats wasteful and i dont do that anymore in taxaHFE-ML, i dont want to do it here.

--- utilities/shap_figures.R
+++ utilities/shap_figures.R
@@ -46,44 +46,12 @@ tryCatch( { if (length(levels(as.factor(input$label))) == 2) {
   sv_full <- shapviz::shapviz(shap_explainations_full, X = shap_data_full)
   
   ## make shap plot
-  importance_plot_full_1 <- shapviz::sv_importance(sv_full, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
+  importance_plot_full <- shapviz::sv_importance(sv_full, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
     ggtitle(label = paste0("SHAP: ", opt$label, " (full data)")) + 
     labs(x = paste0("predictive of ", levels(as.factor(input$label))[2], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[1])) + 
     theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_full_1, filename = paste0(opt$outdir, "importance_plot_full_1.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_full$X)))), 6), height = 4.5, units = "in")
+  ggplot2::ggsave(plot = importance_plot_full, filename = paste0(opt$outdir, "importance_plot_full.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_full$X)))), 6), height = 4.5, units = "in")
   
-  #####################
-  ## FULL DATA 2
-  #####################
-  
-  ## Prediction wrapper
-  pfun <- function(object, newdata) {
-    predict(object, data = newdata)$predictions[, levels(as.factor(input$label))[2]]
-  }
-  
-  ## pull model out of workflow
-  best_workflow <- best_tidy_workflow %>%
-    parsnip::fit(input)
-  best_workflow_mod <- workflows::extract_fit_parsnip(best_workflow)
-  
-  ## pull out data
-  shap_data_full <- recipes::prep(dietML_recipe, input) %>% 
-    recipes::juice() %>% 
-    dplyr::select(-label, -dplyr::any_of(opt$subject_identifier)) %>% 
-    as.matrix()
-  
-  ## explain with fastshap
-  shap_explainations_full <- fastshap::explain(best_workflow_mod$fit, X = shap_data_full, pred_wrapper = pfun, nsim = 100, adjust = TRUE)
-  
-  ## make shap viz object
-  sv_full <- shapviz::shapviz(shap_explainations_full, X = shap_data_full)
-  
-  ## make shap plot
-  importance_plot_full_2 <- shapviz::sv_importance(sv_full, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
-    ggtitle(label = paste0("SHAP: ", opt$label, " (full data)")) + 
-    labs(x = paste0("predictive of ", levels(as.factor(input$label))[1], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[2])) + 
-    theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_full_2, filename = paste0(opt$outdir, "importance_plot_full_2.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_full$X)))), 6), height = 4.5, units = "in")
   
   #####################
   ## TRAIN DATA 1
@@ -112,44 +80,12 @@ tryCatch( { if (length(levels(as.factor(input$label))) == 2) {
   sv_train <- shapviz::shapviz(shap_explainations_train, X = shap_data_train)
   
   ## make shap plot
-  importance_plot_train_1 <- shapviz::sv_importance(sv_train, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
+  importance_plot_train <- shapviz::sv_importance(sv_train, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
     ggtitle(label = paste0("SHAP: ", opt$label, " (train data)")) + 
     labs(x = paste0("predictive of ", levels(as.factor(input$label))[2], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[1])) + 
     theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_train_1, filename = paste0(opt$outdir, "importance_plot_train_1.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_train$X)))), 6), height = 4.5, units = "in")
+  ggplot2::ggsave(plot = importance_plot_train, filename = paste0(opt$outdir, "importance_plot_train.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_train$X)))), 6), height = 4.5, units = "in")
   
-  #####################
-  ## TRAIN DATA 2
-  #####################
-  
-  ## Prediction wrapper: first level (ie if levels are high, low, this is high)
-  pfun <- function(object, newdata) {
-    predict(object, data = newdata)$predictions[, levels(as.factor(input$label))[2]]
-  }
-  
-  ## pull model out of workflow
-  best_workflow <- best_tidy_workflow %>%
-    parsnip::fit(train)
-  best_workflow_mod <- workflows::extract_fit_parsnip(best_workflow)
-  
-  ## pull out data
-  shap_data_train <- recipes::prep(dietML_recipe, train) %>% 
-    recipes::juice() %>% 
-    dplyr::select(-label, -dplyr::any_of(opt$subject_identifier)) %>% 
-    as.matrix()
-  
-  ## explain with fastshap
-  shap_explainations_train <- fastshap::explain(best_workflow_mod$fit, X = shap_data_train, pred_wrapper = pfun, nsim = 100, adjust = TRUE)
-  
-  ## make shap viz object
-  sv_train <- shapviz::shapviz(shap_explainations_train, X = shap_data_train)
-  
-  ## make shap plot
-  importance_plot_train_2 <- shapviz::sv_importance(sv_train, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
-    ggtitle(label = paste0("SHAP: ", opt$label, " (train data)")) + 
-    labs(x = paste0("predictive of ", levels(as.factor(input$label))[1], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[2])) + 
-    theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_train_2, filename = paste0(opt$outdir, "importance_plot_train_2.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_train$X)))), 6), height = 4.5, units = "in")
   
   #####################
   ## TEST DATA 1
@@ -178,44 +114,12 @@ tryCatch( { if (length(levels(as.factor(input$label))) == 2) {
   sv_test <- shapviz::shapviz(shap_explainations_test, X = shap_data_test)
   
   ## make shap plot
-  importance_plot_test_1 <- shapviz::sv_importance(sv_test, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
+  importance_plot_test <- shapviz::sv_importance(sv_test, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
     ggtitle(label = paste0("SHAP: ", opt$label, " (test data)")) + 
     labs(x = paste0("predictive of ", levels(as.factor(input$label))[2], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[1])) + 
     theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_test_1, filename = paste0(opt$outdir, "importance_plot_test_1.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_test$X)))), 6), height = 4.5, units = "in")
+  ggplot2::ggsave(plot = importance_plot_test, filename = paste0(opt$outdir, "importance_plot_test.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_test$X)))), 6), height = 4.5, units = "in")
   
-  #####################
-  ## TEST DATA 2
-  #####################
-  
-  ## Prediction wrapper: second level (ie if levels are high, low, this is low)
-  pfun <- function(object, newdata) {
-    predict(object, data = newdata)$predictions[, levels(as.factor(input$label))[2]]
-  }
-  
-  ## pull model out of workflow
-  best_workflow <- best_tidy_workflow %>%
-    parsnip::fit(test)
-  best_workflow_mod <- workflows::extract_fit_parsnip(best_workflow)
-  
-  ## pull out data
-  shap_data_test<- recipes::prep(dietML_recipe, test) %>% 
-    recipes::juice() %>% 
-    dplyr::select(-label, -dplyr::any_of(opt$subject_identifier)) %>% 
-    as.matrix()
-  
-  ## explain with fastshap
-  shap_explainations_test <- fastshap::explain(best_workflow_mod$fit, X = shap_data_test, pred_wrapper = pfun, nsim = 100, adjust = TRUE)
-  
-  ## make shap viz object
-  sv_test <- shapviz::shapviz(shap_explainations_test, X = shap_data_test)
-  
-  ## make shap plot
-  importance_plot_test_2 <- shapviz::sv_importance(sv_test, kind = "bee", show_numbers = TRUE, bee_width = 0.2, max_display = 10) + 
-    ggtitle(label = paste0("SHAP: ", opt$label, " (test data)")) + 
-    labs(x = paste0("predictive of ", levels(as.factor(input$label))[1], " < SHAP > ", "predictive of ", levels(as.factor(input$label))[2])) + 
-    theme_bw(base_size = 14)
-  ggplot2::ggsave(plot = importance_plot_test_2, filename = paste0(opt$outdir, "importance_plot_test_2.pdf"), width = pmax((0.1 * max(nchar(colnames(sv_test$X)))), 6), height = 4.5, units = "in")
   
   } 
   
